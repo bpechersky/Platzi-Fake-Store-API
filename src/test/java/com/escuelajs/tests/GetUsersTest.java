35e7a421b9f83f1ae374832eda2b2780c12c4ae3
Boris Pechersky <bpechersky@gmx.com>
2025-07-22 16:09:53 -0700

    added testGetUserById test method

--- src/test/java/com/escuelajs/tests/GetUsersTest.java
+++ src/test/java/com/escuelajs/tests/GetUsersTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.Matchers.*;
 import static org.testng.Assert.*;
 
 public class GetUsersTest {
-
+private static int userId;
     @Test
     public void testGetAllUsersWithLimit() {
         Response response = RestAssured
@@ -26,9 +26,34 @@ public class GetUsersTest {
                 .body("[0].email", notNullValue())
                 .extract().response();
 
+         userId = response.path("[0].id");
+
+         System.out.println(("userId is " +userId));
+
         System.out.println("User list response (limit=100):");
         System.out.println(response.asPrettyString());
 
+        assertEquals(response.statusCode(), 200);
+    }
+    @Test(dependsOnMethods = "testGetAllUsersWithLimit")
+    public void testGetUserById() {
+
+
+        Response response = RestAssured
+                .given()
+                .baseUri("https://api.escuelajs.co")
+                .basePath("/api/v1/users/" + userId)
+                .header("accept", "*/*")
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(userId))
+                .body("email", notNullValue())
+                .body("name", notNullValue())
+                .extract().response();
+
+        System.out.println("User Details:\n" + response.asPrettyString());
         assertEquals(response.statusCode(), 200);
     }
 }
